b3f131467092589537541b5eeaf6a969cf1bf3cb
RickyGervais61 <148702921+RickyGervais61@users.noreply.github.com>
2023-10-25 23:49:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const generateBtn = document.querySelector(".generateBtn");
 const imageContainer = document.getElementById("image-container");
-let currentIndex = 0; // 현재 이미지 배열의 인덱스
-const images = ["images/face.png", "images/face2.png"]; // 이미지 파일 경로 배열
+let currentIndex = 0;
+const images = ["images/faca.png", "images/facb.png"];
 
 generateBtn.addEventListener("click", changeImage);
 
@@ -9,11 +9,11 @@ function changeImage() {
   const fromNum = document.querySelector(".fromNum").value;
   const toNum = document.querySelector(".toNum").value;
 
-  // 이미지가 이미 존재하는지 확인
+
   const existingImage = imageContainer.querySelector("img");
 
   if (existingImage) {
-    // 이미지가 이미 존재하면 제거
+
     imageContainer.removeChild(existingImage);
   }
 
@@ -22,11 +22,9 @@ function changeImage() {
   if (randomNum >= parseInt(fromNum)) {
     const randomNumEl = document.querySelector(".randomNumEl");
     const image = document.createElement("img");
-    image.src = images[currentIndex]; // 현재 인덱스에 해당하는 이미지 파일 경로 사용
+    image.src = images[currentIndex]; 
     randomNumEl.innerHTML = randomNum;
-    imageContainer.appendChild(image); // 이미지를 컨테이너에 추가
-
-    // 다음 이미지를 표시하기 위해 인덱스 업데이트
+    imageContainer.appendChild(image); 
     currentIndex = (currentIndex + 1) % images.length;
   }
 }
