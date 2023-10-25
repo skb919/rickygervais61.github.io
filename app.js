005406a5164eb039445467fa9c60fa88398fdc16
RickyGervais61 <148702921+RickyGervais61@users.noreply.github.com>
2023-10-25 23:15:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,21 @@ function generateImage() {
   const fromNum = document.querySelector(".fromNum").value;
   const toNum = document.querySelector(".toNum").value;
 
-  randomNum = Math.floor(Math.random() * parseInt(toNum));
+  // 이미지가 이미 존재하는지 확인
+  const existingImage = imageContainer.querySelector("img");
+
+  if (existingImage) {
+    // 이미지가 이미 존재하면 제거
+    imageContainer.removeChild(existingImage);
+  }
+
+  randomNum = Math.floor(Math.random() * (parseInt(toNum) - parseInt(fromNum) + 1)) + parseInt(fromNum);
 
   if (randomNum >= parseInt(fromNum)) {
     const randomNumEl = document.querySelector(".randomNumEl");
-
-    // 이미지가 이미 존재하는지 확인
-    const existingImage = imageContainer.querySelector("img");
-
-    if (!existingImage) {
-      const image = document.createElement("img");
-      image.src = "images/face.png";
-      randomNumEl.innerHTML = randomNum;
-      imageContainer.appendChild(image); // 이미지를 컨테이너에 추가
-    }
+    const image = document.createElement("img");
+    image.src = "images/face.png";
+    randomNumEl.innerHTML = randomNum;
+    imageContainer.appendChild(image); // 이미지를 컨테이너에 추가
   }
 }
