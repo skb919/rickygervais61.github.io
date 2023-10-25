5bf56c82d4ec92f0b3901c493876efc1ff2b108f
RickyGervais61 <148702921+RickyGervais61@users.noreply.github.com>
2023-10-25 23:11:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const generateBtn = document.querySelector(".generateBtn");
 const imageContainer = document.getElementById("image-container");
-let count = 0; // count 변수를 함수 외부에 위치시킴
 
-generateBtn.addEventListener("click", generateRandomNum);
+generateBtn.addEventListener("click", generateImage);
 
-function generateRandomNum() {
+function generateImage() {
   const fromNum = document.querySelector(".fromNum").value;
   const toNum = document.querySelector(".toNum").value;
 
@@ -12,27 +11,15 @@ function generateRandomNum() {
 
   if (randomNum >= parseInt(fromNum)) {
     const randomNumEl = document.querySelector(".randomNumEl");
-    const image = document.createElement("img"); 
-    image.src = "images/face.png"; 
-    randomNumEl.innerHTML = randomNum;
-    imageContainer.appendChild(image); // 이미지를 컨테이너에 추가
 
-    count++; // count를 증가시킴
-      
-    if (count === 2)
-    { 
-        resetImage();
+    // 이미지가 이미 존재하는지 확인
+    const existingImage = imageContainer.querySelector("img");
+
+    if (!existingImage) {
+      const image = document.createElement("img");
+      image.src = "images/face.png";
+      randomNumEl.innerHTML = randomNum;
+      imageContainer.appendChild(image); // 이미지를 컨테이너에 추가
     }
   }
-}
-
-function resetImage() {
-    // 컨테이너에서 이미지 요소를 찾아서 제거합니다.
-    const image = imageContainer.querySelector("img");
-
-    if (image) {
-        imageContainer.removeChild(image);
-    }
-
-    count = 0; // count를 재설정
 }
