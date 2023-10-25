0e5633244d349382126f6f4a06a13e185ad0a52d
RickyGervais61 <148702921+RickyGervais61@users.noreply.github.com>
2023-10-25 22:56:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const generateBtn = document.querySelector(".generateBtn");
 
 generateBtn.addEventListener("click", generateRandomNum);
 
-function resetImage() {
-    if (imageContainer.contains(image)) {
-        imageContainer.removeChild(image);
-    }
-}
-
 function generateRandomNum() {
   const fromNum = document.querySelector(".fromNum").value;
   const toNum = document.querySelector(".toNum").value;
@@ -18,17 +12,31 @@ function generateRandomNum() {
   if (randomNum >= parseInt(fromNum)) {
     const randomNumEl = document.querySelector(".randomNumEl");
     const image = document.createElement("img"); 
-    let count = 0; 
     image.src = "images/face.png"; 
     randomNumEl.innerHTML = randomNum;
     const imageContainer = document.getElementById("image-container");
     imageContainer.appendChild(image);
-    count ++;
+
+    let count = 0;
+    count++;
+      
     if (count === 2)
     { 
         resetImage();
     }
   }
+}
+
+function resetImage() {
+    // 이미지를 표시할 컨테이너 요소를 선택합니다.
+    const imageContainer = document.getElementById("image-container");
+    
+    // 컨테이너에서 이미지 요소를 찾아서 제거합니다.
+    const image = imageContainer.querySelector("img");
+
+    if (image) {
+        imageContainer.removeChild(image);
+    }
 }
 
 
